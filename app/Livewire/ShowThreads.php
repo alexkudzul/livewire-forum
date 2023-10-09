f15419a7d951ee80cfb988c6b255cbbc312823f2
alexkudzul <alexkudzul@gmail.com>
2023-10-08 20:03:31 -0600

    Added filter by category

--- app/Livewire/ShowThreads.php
+++ app/Livewire/ShowThreads.php
@@ -9,12 +9,18 @@ use App\Models\Category;
 class ShowThreads extends Component
 {
     public $search;
+    public $category;
 
     public function render()
     {
         $categories = Category::get();
         $threads = Thread::query();
         $threads->where('title', 'like', "%$this->search%");
+
+        if ($this->category) {
+            $threads->where('category_id', $this->category);
+        }
+
         $threads->withCount('replies');
         $threads->latest();
 
@@ -22,5 +28,10 @@ class ShowThreads extends Component
             'categories' => $categories,
             'threads' => $threads->get(),
         ]);
+    }
+
+    public function filterByCategory($category)
+    {
+        $this->category = $category;
     }
 }
